598a99424597212a9f0ddd3e6682852d5df665e3
vairam-svs <vairam.svs@outlook.com>
2015-08-05 01:12:24 -0700

    initial c6 sample code

---
+++ c6Sample/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c6Sample
+{
+    public class Person
+    {
+        /// <summary>
+        /// Auto-property initializers
+        /// </summary>
+        public int Id { get; set; } = -1;
+        /// <summary>
+        /// Read-only auto-properties
+        /// </summary>
+        public string Guid { get; } = (new Guid()).ToString();
+        /// <summary>
+        /// Ctor assignment to getter-only autoprops
+        /// see assignment inside the constructor for a getter only property
+        /// </summary>
+        public decimal Salary { get; }
+
+        public Person()
+        {
+            //Ctor assignment to getter-only autoprops
+            Salary = 756;
+        }
+    }
+}
---
+++ c6Sample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Static imports see below writeline and read methods without class prefix.
+using static System.Console;
+namespace c6Sample
+{
+    /// <summary>
+    /// https://github.com/dotnet/roslyn/wiki/Languages-features-in-C%23-6-and-VB-14
+    /// http://davefancher.com/2015/07/17/my-5-favorite-c-6-0-features/
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+            WriteLine(p.Id);
+            WriteLine(p.Guid);
+            WriteLine(p.Salary);
+            Read();
+        }
+    }
+}
